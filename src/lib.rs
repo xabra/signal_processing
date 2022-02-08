44b0e340f8d61bdca4be2c472b2408ec29550a7e
Adam Brailove <adam.brailove@gmail.com>
2022-02-08 12:46:47 -0800

    circular vector working

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,51 @@
-/// Returns the index of a circular array or Vec such that
-/// an index off either end of the array is mapped onto the buffer
-/// Negative indices are handled correctly
-pub fn circular_index(i: i32, n: i32) -> i32 {
-    let mut j = i % n;
-    if j < 0 {
-        j += n
-    };
-    j
+pub struct CircularVector {
+    size: usize,    // Number of elements
+    tail: usize,    // Circular index of the most recently added element
+    data: Vec<f64>, // Vector containing the data
+}
+impl CircularVector {
+    /// Constructor
+    pub fn new(size: usize) -> Self {
+        // Should check for size == 0 here. Error
+        Self {
+            size: size,
+            tail: size - 1, // raw index to the last element
+            data: vec![0.0; size],
+        }
+    }
+    /// Return vector element at index, where index 0 is the head of the vector
+    pub fn get(&self, index: usize) -> f64 {
+        self.data[self.circular_index(self.tail + 1 + index)]
+    }
+
+    /// Push an element at the end (tail) of the vector, popping off the element at the beginning (head) of the vector and returning it.
+    pub fn push(&mut self, x: f64) -> f64 {
+        let head_index = self.circular_index(self.tail + 1); // Head index always follows the tail inxex
+        let head_value = self.data[head_index]; // Save head value to return later
+        self.data[head_index] = x; // Overwrite the head with the new tail value
+        self.tail = head_index; // Update the tail index to point to the new location
+        head_value // Return the old (popped) head value
+    }
+    /// Returns the index of a circular vector or Vec such that an index that overruns the end of the vector is mapped onto the beginning of the vector
+    pub fn circular_index(&self, i: usize) -> usize {
+        i % self.size
+    }
+    /// Debug print raw vector as it is stored internally
+    pub fn print_raw(&self) {
+        for i_raw in 0..self.size {
+            print!("{} ", self.data[i_raw]);
+        }
+        println!("");
+    }
+
+    /// Debug print vector starting from the head
+    pub fn print(&self) {
+        print!("(");
+        for i in 0..self.size {
+            print!("{} ", self.get(i));
+        }
+        println!(")");
+    }
 }
 
 #[cfg(test)]
@@ -14,17 +53,11 @@ mod tests {
     use super::*;
     #[test]
     fn circular_index_positive_index() {
-        let result = circular_index(6, 5);
-        assert_eq!(result, 1);
+        assert_eq!(1, 1);
     }
-    #[test]
-    fn circular_index_negative_index() {
-        let result = circular_index(-6, 5);
-        assert_eq!(result, 4);
-    }
+
     #[test]
     fn circular_index_zero_index() {
-        let result = circular_index(0, 5);
-        assert_eq!(result, 0);
+        assert_eq!(0, 0);
     }
 }
